fa2244d583d4eb3a33bab45e6d9ddaf6b57d673f
Luis Menina <liberforce@freeside.fr>
2025-11-09 16:46:33 +0100

    Add base configuration and build helpers

---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = "liberforce"
+SITENAME = "Greetings From The Free Side!"
+SITEURL = "blog.freeside.fr"
+
+PATH = "content"
+STATIC_PATHS = ["media"]
+
+TIMEZONE = "Europe/Paris"
+
+DEFAULT_LANG = "fr"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("GNOME", "http://www.gnome.org"),
+    ("Mageia", "http://www.mageia.org"),
+)
+
+# Social widget
+SOCIAL = (("Mastodon", "https://framapiaf.org/@liberforce"),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# File location
+ARTICLE_URL = "post/{date:%Y}/{date:%m}/{date:%d}/{slug}/"
+ARTICLE_SAVE_AS = ARTICLE_URL + "index.html"
